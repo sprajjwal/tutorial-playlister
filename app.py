ee2946d1c90cd4a3f39efac9050e019ea4d96220
sprajjwal <sprajjwal@ucdavis.edu>
2019-09-27 14:21:26 -0700

    [ADD]added comments

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 playlists = db.playlists
 
+db = client.get_default_database()
+playlists = db.playlists
+comments = db.comments
 
 app = Flask(__name__)
 
@@ -36,7 +39,8 @@ def playlists_new():
 def playlists_show(playlist_id):
     """Show a single playlist."""
     playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
-    return render_template('playlists_show.html', playlist=playlist)
+    playlist_comments = comments.find({'playlist_id':ObjectId(playlist_id)})
+    return render_template('playlists_show.html', playlist=playlist, comments=playlist_comments)
 
 @app.route('/playlists/<playlist_id>', methods=['POST'])
 def playlists_update(playlist_id):
@@ -63,6 +67,18 @@ def playlists_delete(playlist_id):
     playlists.delete_one({'_id': ObjectId(playlist_id)})
     return redirect(url_for('playlists_index'))
 
+
+@app.route('/playlists/comments', methods=['POST'])
+def comments_new():
+    """Submit a new comment."""
+    comment = {
+        'title': request.form.get('title'),
+        'content': request.form.get('content'),
+        'playlist_id': ObjectId(request.form.get('playlist_id'))
+    }
+    print(comment)
+    comment_id = comments.insert_one(comment).inserted_id
+    return redirect(url_for('playlists_show', playlist_id=comment['playlist_id']))
 
 if __name__ == '__main__':
     # playlists.delete_many({})
