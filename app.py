c2a78236efe3ee8321bd428bf93d8da85dd5c2fb
sprajjwal <sprajjwal@ucdavis.edu>
2019-09-27 16:38:20 -0700

    [FIX]fixed delete playlist button

--- app.py
+++ app.py
@@ -94,6 +94,3 @@ if __name__ == '__main__':
     # playlists.delete_many({})
     # comments.delete_many({})
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
-    
-
-    
